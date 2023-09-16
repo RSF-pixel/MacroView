072ee508a72bdf631b16f0b9833f125fd7c67636
Rúben ferreira <rubenferreira812@gmail.com>
2023-09-16 15:18:12 +0100

    Adding some extra routes

--- macroview/src/app/app-routing.module.ts
+++ macroview/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnalitycsComponent } from './analitycs/analitycs.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'analitycs', component: AnalitycsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
 ];
 
 @NgModule({
--- macroview/src/app/app.module.ts
+++ macroview/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AnalitycsComponent } from './analitycs/analitycs.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    AnalitycsComponent
+    AnalitycsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
