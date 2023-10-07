905970fa1436e94f46c3db9b10c4547159dec1b1
Rúben ferreira <rubenferreira812@gmail.com>
2023-10-07 21:07:03 +0100

    Fixing project

--- macroview/src/app/portfolio/portfolio.component.ts
+++ macroview/src/app/portfolio/portfolio.component.ts
@@ -9,26 +9,27 @@ declare var TradingView: any;
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
+
 export class PortfolioComponent implements AfterViewInit {
 
 
   ngAfterViewInit() {
     // Initialize the TradingView widget here
-    new TradingView.widget({
-      // Configuration options go here
-      container_id: 'tradingview-widget-container',
-      symbol: 'AAPL',
-      interval: 'D', // Daily interval, adjust as needed
-      timezone: 'Etc/UTC', // Set timezone accordingly
-      theme: 'light', // or 'dark'
-      style: '1', // or '2' for bars/candles
-      enable_publishing: false, // Allow publishing chart data
-      withdateranges: true, // Show date range picker
-      hide_side_toolbar: false, // Show side toolbar
-      allow_symbol_change: true, // Allow changing symbols
-      save_image: false, // Allow saving chart images
-      gridColor: "rgba(251, 247, 240, 1)",
-    });
+    // new TradingView.widget({
+    //   // Configuration options go here
+    //   container_id: 'tradingview-widget-container',
+    //   symbol: 'AAPL',
+    //   interval: 'D', // Daily interval, adjust as needed
+    //   timezone: 'Etc/UTC', // Set timezone accordingly
+    //   theme: 'light', // or 'dark'
+    //   style: '1', // or '2' for bars/candles
+    //   enable_publishing: false, // Allow publishing chart data
+    //   withdateranges: true, // Show date range picker
+    //   hide_side_toolbar: false, // Show side toolbar
+    //   allow_symbol_change: true, // Allow changing symbols
+    //   save_image: false, // Allow saving chart images
+    //   gridColor: "rgba(251, 247, 240, 1)",
+    // });
   }
 
 }
