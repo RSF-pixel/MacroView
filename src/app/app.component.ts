6376193e2b7c8a7193565378b114a7387c92d008
Rúben ferreira <rubenferreira812@gmail.com>
2023-12-17 15:53:54 +0000

    Setup firebase into the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { environment } from '../../enviroments/enviroment';
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -15,7 +19,29 @@ export class AppComponent {
     this.navigateToDefaultComponent()
   }
 
-  navigateToDefaultComponent() {
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: environment.firebaseConfig.FIREBASE_API_KEY,
+      authDomain: environment.firebaseConfig.FIREBASE_AUTH_DOMAIN,
+      projectId: environment.firebaseConfig.FIREBASE_PROJECT_ID,
+      storageBucket: environment.firebaseConfig.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: environment.firebaseConfig.FIREBASE_MESSAGING_SENDER_ID,
+      appId: environment.firebaseConfig.FIREBASE_APP_ID,
+      measurementId: environment.firebaseConfig.FIREBASE_MEASUREMENT_ID
+    };
+    
+    
+    // Initialize Firebase
+    
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    console.log(analytics);
+    
+    
+  }
+
+  navigateToDefaultComponent() : void {
     this.router.navigate(['/dashboard']);
   }
 
